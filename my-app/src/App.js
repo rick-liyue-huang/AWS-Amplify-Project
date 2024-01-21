dd0b04ffd18a85f9fb60bf8c21bf545782406d72
Rick Huang <rick.liyue.huang@gmail.com>
2024-01-21 11:49:54 +1100

    feat: add contact page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import { HomeComponent } from './components/home/HomeComponent';
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from './components/auth/LoginPage';
 import { RegisterPage } from './components/auth/RegisterPage';
+import { ContactPage } from './components/contacts/Contact';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomeComponent />} />
         <Route path='/about' element={<div>About</div>} />
-        <Route path='/contact' element={<div>Contact</div>} />
+        <Route path='/contact' element={<ContactPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
       </Routes>
